13018c662a8f5c0408a96e3fd8bc8ed92337c22f
Miguel Jimenez <migijim@hotmail.com>
2021-08-19 14:47:59 +0200

    new

--- src/App.js
+++ src/App.js
@@ -30,6 +30,13 @@ function App() {
       phone :"123456", 
       email : "hello@test.com"}} 
       />
+            <ContactCard 
+      
+      contact={{name: "Mr. Whiskerson", 
+      imUrl : "http://www.w3.org/2000/svg", 
+      phone :"123456", 
+      email : "hello@test.com"}} 
+      />
     </div>
   );
 }
